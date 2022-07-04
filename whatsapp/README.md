c468ac8243b9b1efb3ad08a0ae997f7ee74bd3b7
Muhammad Ansori Nasution <muhammadansorinasution@gmail.com>
2022-07-04 15:11:00 +0700

    first commit

---
+++ whatsapp/README.md
@@ -0,0 +1,31 @@
+whatsappweb
+===========
+whatsappweb
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist whatsappweb/yii2-whatsapp-web "*"
+```
+
+or add
+
+```
+"whatsappweb/yii2-whatsapp-web": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \codinglab\whatsappweb\AutoloadExample::widget(); ?>```
---
+++ whatsapp/src/WhatsAppWeb.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace codinglab\whatsapp;
+
+use yii\base\Component;
+
+class WhatsAppWeb extends Component
+{
+    private $url; // = 'send-message';
+    private $phoneNumber; // ';
+    private $message = '';
+    private $clientId = 0;
+
+    function __construct($url, $phoneNumber, $message, $clientId)
+    {
+        $this->url = $url;
+        $this->phoneNumber = $phoneNumber;
+        $this->message = $message;
+        $this->clientId = $clientId;
+    }
+
+    function sendMessage()
+    {
+
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_URL => $this->url,
+            CURLOPT_HTTPHEADER => [
+                'Content-Type: application/json'
+            ],
+            CURLOPT_POSTFIELDS =>  json_encode([
+                "number" => $this->phoneNumber,
+                "message" => $this->message,
+                "sender" => $this->clientId,
+            ])
+        ]);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        print  $server_output;
+    }
+}
